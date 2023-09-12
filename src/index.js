8d04ca4d0a48d21bb3199f045c3cb52bf4ff4798
XxLiosxX <ybarraroberto82@gmail.com>
2023-09-12 20:05:36 +0200

    aaa

--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
-require('dotenv').config()
 const express = require('express');
-const app = express()
-const path = require('path');
+const app = express();
+const dotenv = require('dotenv');
 const cors = require('cors');
-const connection = require('./connection');
-const contactoRouter = require('./router/contact');
+const path = require('path');
+
+dotenv.config();
+
 
 // Configuracion de la aplicación
 
-app.set('title', 'Mi Portafolio')
-app.set('port', process.env.PORT || 3000)
-app.set('view engine', 'pug')
-app.set('views', path.join(__dirname, 'views'))
+app.set('title', 'Mi Portafolio');
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
-//middlewares
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.urlencoded({ extended: true, }))
-app.use(express.json())
-app.use(cors())
+// middlewares
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true, }));
+app.use(express.json());
 
 // Rutas
 app.get('/portfolio', (req, res) => {
-    res.render('index')
-})
+    res.render('index');
+});
 
-app.use('/', contactoRouter);
 
 // Iniciazion del servidor
 
 app.listen(app.get('port'), () => {
     console.log(`Mi servidor esta corriendo en: localhost:` + app.get('port'));
-})
+});
